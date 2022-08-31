5363365e45bf02d67727d94c3e92e4497e00ccaa
brunaverrone <bruna.verrone@sumup.com>
2022-08-31 17:58:19 -0300

    Finalização da Aula 6

--- 6-listas.js
+++ 6-listas.js
@@ -20,6 +20,9 @@ listaDeDestinos.push(`Vitória`); //adicionando um item na lista
 console.log(listaDeDestinos);
 
 listaDeDestinos.splice(3, 1); //o comando splice é o responsavel por deletar o dado da lista
-//o numero 3 é a posicao do elemento que eu quero deletar - e comeca a contar da posicao 0
-//o numero 1 é a quantidade de elementos que eu quero deletar
+//o numero [3] é a posicao do elemento que eu quero deletar*
+//o numero [1] é a quantidade de elementos que eu quero deletar
 console.log(listaDeDestinos);
+
+//Para acessar um elemento especifico podemos chamar o nome dessa lista seguido de colchetes com o index desse elemento. Ex: lista[2]
+//*As listas sempre começam a contagem de elementos a partir do 0, então o index 2 mostra o terceiro item da lista!
