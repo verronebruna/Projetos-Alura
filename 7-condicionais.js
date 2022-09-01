70f133b5564b366ad796016ff329ccdcf6ada03c
brunaverrone <bruna.verrone@sumup.com>
2022-09-01 17:39:08 -0300

    atualização da Aula 7

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -4,19 +4,19 @@ console.log(`Trabalhando com condicionais`);
 const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 
 const idadeComprador = 14;
+const estaAcompanhada = true;
+
 console.log("Destinos possiveis: ");
 console.log(listaDeDestinos);
 
 if (idadeComprador >= 18) {
   console.log("Comprador é maior de idade.");
   listaDeDestinos.splice(1, 1); //removendo item que a pessoa quer comprar (impedindo a compra)
+} else if (estaAcompanhada) {
+  console.log("Comprador é maior de idade.");
+  listaDeDestinos.splice(1, 1); //removendo item
 } else {
-  if (estaAcompanhada) {
-    console.log("Comprador é maior de idade.");
-    listaDeDestinos.splice(1, 1); //removendo item
-  } else {
-    console.log("Comprador não é maior de idade e não posso vender.");
-  }
+  console.log("Comprador não é maior de idade e não posso vender.");
 }
 
 console.log(listaDeDestinos);
