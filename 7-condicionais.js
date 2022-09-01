0e23661680ea72bb6f4ab971a233bc42a5a28dc6
brunaverrone <bruna.verrone@sumup.com>
2022-09-01 17:48:57 -0300

    atualização da Aula 7

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -9,15 +9,23 @@ const estaAcompanhada = true;
 console.log("Destinos possiveis: ");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
+// if (idadeComprador >= 18) {
+//   console.log("Comprador é maior de idade.");
+//   listaDeDestinos.splice(1, 1); (removendo item que a pessoa quer comprar)
+// } else if (estaAcompanhada) {
+//   console.log("Comprador é maior de idade.");
+//   listaDeDestinos.splice(1, 1); (removendo item) - evite ter código duplicado!
+// } else {
+//   console.log("Comprador não é maior de idade e não posso vender.");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada) {
   console.log("Comprador é maior de idade.");
-  listaDeDestinos.splice(1, 1); //removendo item que a pessoa quer comprar (impedindo a compra)
-} else if (estaAcompanhada) {
-  console.log("Comprador é maior de idade.");
-  listaDeDestinos.splice(1, 1); //removendo item
+  listaDeDestinos.splice(1, 1); //removendo item que a pessoa quer comprar
 } else {
   console.log("Comprador não é maior de idade e não posso vender.");
 }
+//Para escrevermos algo como "ou" está acompanhado ou é maior de idade para fazer compra de pacotes, utilizamos ||no JavaScript
 
 console.log(listaDeDestinos);
 
